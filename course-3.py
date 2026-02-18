460c33c40ed59ef58ead3a52156fb19fb032bd96
AditiBhandari <aditi.bhandari@ft.com>
2026-02-18 14:07:57 +0000

    added course 3 module 3

--- course-3.py
+++ course-3.py
@@ -1,21 +1,37 @@
 ### MODULE 2 EXTRACTING DATA WITH REGULAR EXPRESSIONS
 
-import re
+# import re
 
-filename = open("regex_sum_2363595.txt","r")
+# filename = open("regex_sum_2363595.txt","r")
 
-total = list()
+# total = list()
 
-for line in filename:
-	line = line.strip()
-	findNum = re.findall('([0-9]+)',line)
-	if len(findNum) >= 1:
-		# for number in findNum:
-		# 	number = float(number)
-		# This doesn't work because it doesn't write the converted numbers back into the list
-		findNum = list(map(int, findNum))
-		sumNum = sum(findNum)
-		total.append(sumNum)
-print(sum(total))
+# for line in filename:
+# 	line = line.strip()
+# 	findNum = re.findall('([0-9]+)',line)
+# 	if len(findNum) >= 1:
+# 		# for number in findNum:
+# 		# 	number = float(number)
+# 		# This doesn't work because it doesn't write the converted numbers back into the list
+# 		findNum = list(map(int, findNum))
+# 		sumNum = sum(findNum)
+# 		total.append(sumNum)
+# print(sum(total))
 
 
+### MODULE 3 UNDERSTANDING THE REQUEST / RESPONSE CYCLE
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
