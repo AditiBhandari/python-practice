c03dee89ded0bc233eff55db34913724aafb6be2
AditiBhandari <aditi.bhandari@ft.com>
2026-02-25 14:07:20 +0000

    upload beautifulsoup

---
+++ beautifulSoupPractice/main.py
@@ -0,0 +1,52 @@
+def main():
+
+    # ### Module 4 Assignment 1
+
+    # from urllib.request import urlopen
+    # from bs4 import BeautifulSoup
+    # import ssl
+
+    # # Ignore SSL certificate errors
+    # ctx = ssl.create_default_context()
+    # ctx.check_hostname = False
+    # ctx.verify_mode = ssl.CERT_NONE
+
+    # url = input("Enter - ")
+    # html = urlopen(url,context=ctx).read()
+    # soup = BeautifulSoup(html,"html.parser")
+
+    # # Retrieve all the anchor tags
+    # tags = soup("span")
+
+    # counter = 0
+
+    # for tag in tags:
+    #     if counter == 0:
+    #         counter = int(tag.contents[0])
+    #     else:
+    #         counter = counter + int(tag.contents[0])
+    # print(counter)
+
+
+    ### Module 4 Assignment 2
+    from urllib.request import urlopen
+    from bs4 import BeautifulSoup
+    import ssl
+
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+
+    url = input("Enter url:")
+    html = urlopen(url, context=context).read()
+    soup = BeautifulSoup(html, "html.parser")
+    
+    anchorTags = soup("a")
+
+    for tag in anchorTags:
+        print(tag)
+
+
+
+if __name__ == "__main__":
+    main()
